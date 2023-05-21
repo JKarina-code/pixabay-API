20cdc57500f4714477ab3b4f5a4f39002d8b973d
Jymma <jkmogollon.cp@gmail.com>
2023-05-21 17:36:59 -0500

    Moving project to async await

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 const result = document.querySelector("#result");
 const formPixa = document.querySelector("#formPixa");
+const paginationDiv = document.querySelector("#pagination");
+
+let registrePage = 40;
 let actualPage = 1;
+let totalPages;
+let iterator;
 window.onload = () => {
   formPixa.addEventListener("submit", validateForm);
 };
@@ -12,9 +17,9 @@ function validateForm(e) {
 
   if (termSearch === "") {
     showAlert("Please add a search term");
+    return;
   }
-
-  searchImages(termSearch);
+  searchImages();
 }
 
 function showAlert(message) {
@@ -47,18 +52,44 @@ function showAlert(message) {
   }
 }
 
-function searchImages(termSearch) {
+async function searchImages() {
+  const termSearch = document.querySelector("#term").value;
   const key = "36301233-bbb99c791a41aa0581d1dbb44";
-  const url = `https://pixabay.com/api/?key=${key}&q=${termSearch}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => showImages(data.hits));
+  const url = `https://pixabay.com/api/?key=${key}&q=${termSearch}&per_page=${registrePage}&page=${actualPage}`;
+
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    totalPages = numberPages(result.totalHits);
+    showImages(result.hits);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function showImages(images = []) {
+function* createPaginator(total) {
+  for (let i = 1; i <= total; i++) {
+    yield i;
+  }
+}
+function showImages(images) {
   while (result.firstChild) {
     result.removeChild(result.firstChild);
   }
+  const heading = document.createElement("H2");
+  heading.classList.add(
+    "text-center",
+    "text-white",
+    "mx-auto",
+    "mt-10",
+    "font-bold",
+    "justify-center",
+    "text-3xl"
+  );
+  heading.textContent = images.length
+    ? ""
+    : "No Results, please adding a new term";
+  result.appendChild(heading);
 
   images.forEach((image) => {
     const { likes, views, previewURL, largeImageURL } = image;
@@ -76,4 +107,41 @@ function showImages(images = []) {
     </div>
 `;
   });
+
+  while (paginationDiv.firstChild) {
+    paginationDiv.removeChild(paginationDiv.firstChild);
+  }
+  printPaginator();
+}
+function numberPages(total) {
+  return parseInt(Math.ceil(total / 30));
+}
+function printPaginator() {
+  iterator = createPaginator(totalPages);
+
+  while (true) {
+    const { value, done } = iterator.next();
+    if (done) return;
+
+    // Create button
+    const buttonNext = document.createElement("a");
+    buttonNext.href = "#";
+    buttonNext.dataset.page = value;
+    buttonNext.textContent = value;
+    buttonNext.classList.add(
+      "next",
+      "bg-yellow-400",
+      "px-4",
+      "py-1",
+      "mr-2",
+      "mb-10",
+      "font-bold",
+      "rounded"
+    );
+    buttonNext.onclick = () => {
+      actualPage = value;
+      searchImages();
+    };
+    paginationDiv.appendChild(buttonNext);
+  }
 }
